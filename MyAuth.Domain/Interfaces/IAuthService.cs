9b2bc0dafeb04edf20235885e2942b8326a84fb2
MathsPrado <matheusprado55@gmail.com>
2026-01-12 22:18:54 -0300

    Added

---
+++ MyAuth.Domain/Interfaces/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿using MyAuth.Domain.Dtos;
+
+namespace MyAuth.Domain.Interfaces;
+
+public interface IAuthService
+{
+    Task<string?> LoginAsync(string username, string password);
+    Task<UserDto?> GetUserFromTokenAsync(string token);
+}
---
+++ MyAuth.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using MyAuth.Domain.Entities;
+
+namespace MyAuth.Domain.Interfaces;
+
+public interface IUserRepository
+{
+    Task<User?> GetByUsernameAsync(string username);
+    Task<User?> GetByIdAsync(int id);
+}
